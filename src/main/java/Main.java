4a0b64089b3d3a43541ab0d4ee0cf825adf30149
Iceneo <nosminin@rit-it.com>
2023-09-22 20:35:39 +0500

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,8 @@
-import entities.Parrot;
-import entities.Person;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        var context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
 
-        var person =
-                context.getBean(Person.class);
-        var parrot =
-                context.getBean(Parrot.class);
-        System.out.println(
-                "Person's name: " + person.getName());
-        System.out.println(
-                "Parrot's name: " + parrot.getName());
-        System.out.println(
-                "Person's parrot: " + person.getParrot());
     }
 }
